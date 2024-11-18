0ae28ff4a2bd429d559cf805bde0dc05de2fd670
Shieng <137869411+Shiengg@users.noreply.github.com>
2024-11-18 20:56:24 +0700

    Fix Toggle Button and Datatable

--- Hotel_Management/Hotel_Management/Controllers/BookingController.cs
+++ Hotel_Management/Hotel_Management/Controllers/BookingController.cs
@@ -4,6 +4,12 @@ namespace Hotel_Management.Controllers
 {
     public class BookingController : Controller
     {
+        private readonly ILogger<BookingController> _logger;
+        public BookingController(ILogger<BookingController> logger)
+        {
+            _logger = logger;
+        }
+
         public IActionResult Index()
         {
             return View();
