bc86735cc0f7f253fe82053da3e926fc70a17b2c
Shieng <137869411+Shiengg@users.noreply.github.com>
2024-11-28 23:15:11 +0700

    Update SQL and Customer API

---
+++ Hotel_Management/Hotel_Management_API/Controllers/CustomerController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Data;
+using Microsoft.EntityFrameworkCore;
+using Data.Models;
+
+namespace Hotel_Management_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CustomerController : ControllerBase
+    {
+        private readonly ILogger<CustomerController> _logger;
+        private readonly DatabaseContext _context;
+
+        // Khởi tạo controller với dependency injection
+        public CustomerController(ILogger<CustomerController> logger, DatabaseContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                var customers = _context.Customer
+                    .Select(u => new
+                    {
+                        u.CustomerID,
+                        u.FullName,
+                        u.PhoneNumber,
+                        u.Email,
+                        u.CCCD,
+                        u.DateOfBirth
+                    });
+                return Ok(customers);
+            }
+            catch (Exception ex)
+            {
+                // Xử lý lỗi nếu có
+                return StatusCode(500, new { message = ex.Message });
+            }
+        }
+    }
+}
