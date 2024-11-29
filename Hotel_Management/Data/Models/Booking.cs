983a47b9bb1c97eec9a03d27af75efdd75ce2f59
Shieng <137869411+Shiengg@users.noreply.github.com>
2024-11-29 20:23:13 +0700

    Update Admin Customer View

--- Hotel_Management/Data/Models/Booking.cs
+++ Hotel_Management/Data/Models/Booking.cs
@@ -5,17 +5,17 @@
         public int BookingID { get; set; }
         // Liên kết đến khách hàng
         public int CustomerID { get; set; }
-        public Customer Customer { get; set; }
+        public required Customer Customer { get; set; }
 
         // Liên kết đến phòng
         public int RoomID { get; set; }
-        public Rooms Room { get; set; }
+        public required Rooms Room { get; set; }
         public DateTime? CheckInDate { get; set; }
         public DateTime? CheckOutDate { get; set; }
-        public string Status { get; set; }
-		public string CustomerName { get; set; } // Thêm trường này
+        public required string Status { get; set; }
+		public required string CustomerName { get; set; } // Thêm trường này
 
-		public string RoomNumber { get; set; } // Thêm trường này
+		public required string RoomNumber { get; set; } // Thêm trường này
 
     }
 }
